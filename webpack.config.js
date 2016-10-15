b16b147116d452faa6a91d09553e99732bd06915
Samuel Navarrete <samueln.contact@gmail.com>
2016-10-15 15:30:27 -0700

    🍕

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+
+module.exports = {
+
+   entry: {
+      javascript: "./app/index.js",
+      html: "./app/index.html"
+
+   },
+   output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js"
+   },
+   devServer: {
+      hot: true
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"       
+         },
+         {
+            test: /\.html/,
+            loader: "file?name=[name].[ext]"
+         },
+      ]
+   }
+
+}
