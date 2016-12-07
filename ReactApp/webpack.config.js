83d777a67e7f1c18bdb9cbf6e609b4d45b5ada69
Samuel Navarrete <samueln.contact@gmail.com>
2016-12-07 14:58:10 -0800

    Add more production options

--- ReactApp/webpack.config.js
+++ ReactApp/webpack.config.js
@@ -1,32 +1,81 @@
 var path = require('path');
+var webpack = require('webpack');
+var CompressionPlugin = require("compression-webpack-plugin");
+
+
+var getDebug = function () {
+    return (process.env.NODE_ENV === 'production');
+};
+
+
+var getSourcemap = function () {
+    if (process.env.NODE_ENV === 'production') {
+        return '#cheap-source-map';
+    }
+
+    return '#eval-source-map';
+};
+
+
+var getPlugins = function() {
+    var plugins = [];
+
+    if (process.env.NODE_ENV === 'production') {
+        plugins.push(new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }));
+        plugins.push(new webpack.optimize.DedupePlugin());
+        plugins.push(new webpack.optimize.UglifyJsPlugin());
+        plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+
+        /*
+        plugins.push(new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        }));
+        */
+    }
+
+    return plugins;
+};
 
 
 module.exports = {
-   entry: './app/index', 
-   output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "bundle.js"
-   },
-   devServer: {
-      contentBase: './app',
-      hot: true
-   },
-
-   module: {
-      loaders: [
-         {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"       
-         },
-         {
-            test: /\.html/,
-            loader: "file?name=[name].[ext]"
-         },
-         {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
-         },
-      ]
-   },
-}
+    entry: {
+        app: './app/index',
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].js"
+    },
+    devServer: {
+        contentBase: './app',
+        hot: true,
+        historyApiFallback: true
+    },
+    debug: getDebug(),
+    devtool: getSourcemap(),
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html/,
+                loader: "file?name=[name].[ext]"
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+            },
+        ]
+    },
+    plugins: getPlugins()
+};
